b03abd9e779ef1363f6da8babfcf70b55f8eb7b1
wrisr <warissat@scg.com>
2026-05-20 01:58:50 +0700

    Update chart-index.js

--- js/chart-index.js
+++ js/chart-index.js
@@ -2264,7 +2264,7 @@ new Chart(ctxrvn, {
                     return context.dataset.backgroundColor;
                 },
                 borderRadius: 4,
-                //formatter: Math.round,
+                formatter: Math.round,
                 color: 'white',
                 padding: 0
             }
